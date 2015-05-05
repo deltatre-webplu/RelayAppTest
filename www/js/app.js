3e3274c91e835bcc3853afb653b7b620f0958a4b
Davide Icardi <davide.icardi@gmail.com>
2015-05-05 15:10:11 +0000

    added plugin

--- www/js/app.js
+++ www/js/app.js
@@ -3,56 +3,30 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ngCordova'])
-.run(function($ionicPlatform, $cordovaPush, $rootScope) {
+angular.module('RelayAppTest', ['ionic', 'RelayAppTest.Controllers'])
+  .run(function($ionicPlatform, $cordovaPush, $rootScope) {
 
-  var androidConfig = {
-    "senderID": "webplud3project",
-  };
-  
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-
-    $cordovaPush.register(androidConfig).then(function(result) {
-      // Success
-       alert('result = ' + result);
-    }, function(err) {
-      // Error
-      
-      alert('error = ' + err);
-    });
-    
-    
-    $rootScope.$on('$cordovaPush:notificationReceived', function(event, notification) {
-      switch(notification.event) {
-        case 'registered':
-          if (notification.regid.length > 0 ) {
-            alert('registration ID = ' + notification.regid);
-          }
-          break;
-
-        case 'message':
-          // this is the actual push notification. its format depends on the data model from the push server
-          alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
-          break;
-
-        case 'error':
-          alert('GCM error = ' + notification.msg);
-          break;
-
-        default:
-          alert('An unknown GCM event has occurred');
-          break;
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
       }
     });
+  })
 
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
 
+  .state('home', {
+    url: "/home",
+    templateUrl: "templates/home.html",
+    controller: 'HomeCtrl'
   });
+  
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
 });
