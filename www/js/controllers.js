a712dea7bb2f82f1c2f7431fe8cff442771e3663
Davide Icardi <davide.icardi@gmail.com>
2015-05-05 15:48:10 +0000

    added plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,57 +4,111 @@ angular.module('RelayAppTest.Controllers', ['ngCordova'])
     .controller('HomeCtrl', function($cordovaPush, $rootScope, $scope) {
 
         var androidConfig = {
-            "senderID": "webplud3project",
+            senderID: "webplud3project"
         };
 
         $scope.registering = false;
-        
-        $scope.register = function(){
+
+        $scope.register = function() {
             $scope.registering = true;
             try
             {
-            $cordovaPush.register(androidConfig).then(function(result) {
-                // Success
-                alert('result = ' + result);
-                $scope.registering = false;
-            }, function(err) {
-                // Error
+                var pushNotification = window.plugins.pushNotification;
+                pushNotification.register(
+                    function(result) {
+                        // Success
+                        alert('result = ' + result);
+                        $scope.registering = false;
+                    },
+                    function(err) {
+                        // Error
     
-                alert('error = ' + err);
-                $scope.registering = false;
-            });
+                        alert('error = ' + err);
+                        $scope.registering = false;
+                    }, {
+                        "senderID": androidConfig.senderID,
+                        "ecb": "onNotificationGCM"
+                    });
             }
             catch (ex)
             {
                 alert('exception = ' + ex);
                 $scope.registering = false;
-            }
+            }                    
         };
 
-        $scope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+        /*        
+                $scope.register = function(){
+                    $scope.registering = true;
+                    try
+                    {
+                    $cordovaPush.register(androidConfig).then(function(result) {
+                        // Success
+                        alert('result = ' + result);
+                        $scope.registering = false;
+                    }, function(err) {
+                        // Error
             
-            alert("notificationReceived" + angular.toJson(notification));
-            
-            switch (notification.event) {
-                case 'registered':
-                    if (notification.regid.length > 0) {
-                        alert('registration ID = ' + notification.regid);
+                        alert('error = ' + err);
+                        $scope.registering = false;
+                    });
                     }
-                    break;
+                    catch (ex)
+                    {
+                        alert('exception = ' + ex);
+                        $scope.registering = false;
+                    }
+                };
 
-                case 'message':
-                    // this is the actual push notification. its format depends on the data model from the push server
-                    alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
-                    break;
+                $scope.$on('$cordovaPush:notificationReceived', function(event, notification) {
+                    
+                    alert("notificationReceived" + angular.toJson(notification));
+                    
+                    switch (notification.event) {
+                        case 'registered':
+                            if (notification.regid.length > 0) {
+                                alert('registration ID = ' + notification.regid);
+                            }
+                            break;
 
-                case 'error':
-                    alert('GCM error = ' + notification.msg);
-                    break;
+                        case 'message':
+                            // this is the actual push notification. its format depends on the data model from the push server
+                            alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+                            break;
 
-                default:
-                    alert('An unknown GCM event has occurred');
-                    break;
-            }
-        });
+                        case 'error':
+                            alert('GCM error = ' + notification.msg);
+                            break;
 
+                        default:
+                            alert('An unknown GCM event has occurred');
+                            break;
+                    }
+                });
+        */
     });
+
+function onNotificationGCM(notification) {
+    alert("notificationReceived" + angular.toJson(notification));
+
+    switch (notification.event) {
+        case 'registered':
+            if (notification.regid.length > 0) {
+                alert('registration ID = ' + notification.regid);
+            }
+            break;
+
+        case 'message':
+            // this is the actual push notification. its format depends on the data model from the push server
+            alert('message = ' + notification.message + ' msgCount = ' + notification.msgcnt);
+            break;
+
+        case 'error':
+            alert('GCM error = ' + notification.msg);
+            break;
+
+        default:
+            alert('An unknown GCM event has occurred');
+            break;
+    }
+}
